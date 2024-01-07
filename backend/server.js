514c6a4fe8e695d6a6f8aeb197138668627f5649
anub-dota <d.anubhab@iitg.ac.in>
2024-01-07 20:01:47 +0530

    registration sucess

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const cors = require('cors')
+const dotenv = require('dotenv').config()
+const bcrypt = require('bcryptjs')
+
+const User = require('./model/user')
+const Channel = require('./model/channels')
+const Message = require('./model/messages')
+const url = process.env.MONGO_URL
+
 
 const app = express()
 app.use(express.json())
@@ -10,29 +17,40 @@ app.use(cors({
     credentials: true,
 }))
 
-const url = "mongodb+srv://littleBob:bigBob@chatapp.sg6qsvv.mongodb.net/?retryWrites=true&w=majority"
-const connectionParams = {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}
-
-mongoose.connect(url,connectionParams)
+console.log(url)
+mongoose.connect(url)
     .then(()=>{
         console.log("Successfully connected to database")
     })
     .catch((err)=>{
-        console.log(err)
+        console.log(err.message)
         console.log("Connection failed")
     })
-app.get('/',(req,res)=>{
-    res.send("Welcome to chat app")
+
+
+app.post('/register', async (req, res) => {
+    // console.log("data sent from front")
+    const {user, pwd} = req.body;
+    //check from database if user exists
+    const password = await bcrypt.hash(pwd, 10)
+
+    try{
+        const response = await User.create({
+            'username':user,
+            'password':password
+        })
+        console.log(response)
+
+    }catch (error){
+        if(error.code ===11000){
+            res.status(409).send()
+        }
+        throw error
+    }
+
+    res.send()
 })
 
-app.post('/register',(req,res)=>{
-    console.log("data sent from front")
-    console.log(req.body)
 
-    res.send({})
-})
 
 app.listen(3500)
